45b6226526ebaf3ec471b04ffefb222835d378ae
jurisconsultostrava <jurisconsultostrava@gmail.com>
2026-08-12 19:13:41 +0200

    Update server to listen on all interfaces

--- server.js
+++ server.js
@@ -103,8 +103,7 @@ app.get('/', (req, res) => {
 </html>
     `);
 });
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(\`Konzole běží na portu: \${PORT}\`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Konzole běží na portu: ${PORT} (prijima externi spojeni)`);
 });
