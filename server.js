5fc0af96f4283c8be52848380843743084dfae71
jurisconsultostrava <jurisconsultostrava@gmail.com>
2026-08-12 19:17:37 +0200

    Change server port to 3000 and bind to 0.0.0.0

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.get('/', (req, res) => {
 </html>
     `);
 });
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, '8.0.8.0', () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Konzole běží na portu: ${PORT} (prijima externi spojeni)`);
 });
